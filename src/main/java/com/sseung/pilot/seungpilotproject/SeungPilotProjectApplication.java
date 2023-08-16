dcb2030adb59419ae705cb8c52c84595363f0a50
이승혜 <seunghye@iseunghyeui-MacBookPro.local>
2023-08-16 13:54:34 +0900

    Initial commit

---
+++ src/main/java/com/sseung/pilot/seungpilotproject/SeungPilotProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sseung.pilot.seungpilotproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeungPilotProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeungPilotProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sseung/pilot/seungpilotproject/SeungPilotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sseung.pilot.seungpilotproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeungPilotProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
