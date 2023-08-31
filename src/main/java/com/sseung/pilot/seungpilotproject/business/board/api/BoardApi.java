23700297259ad1fd62d310655a2ac14a452fd0ff
sseunghye-lee <laurel8310@naver.com>
2023-08-31 16:31:02 +0900

    swagger authorization 추가

--- src/main/java/com/sseung/pilot/seungpilotproject/business/board/api/BoardApi.java
+++ src/main/java/com/sseung/pilot/seungpilotproject/business/board/api/BoardApi.java
@@ -5,6 +5,7 @@ import com.sseung.pilot.seungpilotproject.commons.dto.request.board.BoardRequest
 import com.sseung.pilot.seungpilotproject.commons.dto.response.board.BoardResponse;
 import com.sseung.pilot.seungpilotproject.commons.utils.ApiUtils.ApiResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import static com.sseung.pilot.seungpilotproject.commons.utils.ApiUtils.success;
@@ -15,11 +16,13 @@ import static com.sseung.pilot.seungpilotproject.commons.utils.ApiUtils.success;
 public class BoardApi {
     private final BoardService boardService;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping
     public ApiResult<BoardResponse> add(@RequestBody BoardRequest boardRequest) {
         return success(boardService.insertBoard(boardRequest));
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/{bdId}")
     public ApiResult<BoardResponse> getBoard(@PathVariable("bdId") long bdId) {
         return success(boardService.findBoard(bdId));
---
+++ src/main/java/com/sseung/pilot/seungpilotproject/commons/resolver/LoginUserDetailsResolver.java
@@ -0,0 +1,30 @@
+package com.sseung.pilot.seungpilotproject.commons.resolver;
+
+import com.sseung.pilot.seungpilotproject.commons.security.SecurityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class LoginUserDetailsResolver implements HandlerMethodArgumentResolver {
+
+    private final SecurityService securityService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginUser.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+        ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest,
+        WebDataBinderFactory binderFactory) {
+        return securityService.getAuthentication();
+    }
+}
